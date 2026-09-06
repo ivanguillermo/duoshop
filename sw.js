8ecca8f14ab8a586a87dc66295dc206fd17d50b2
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-09-06 14:51:12 -0400

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,31 +1,30 @@
 const CACHE_NAME = 'duo-shop-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/duoshop.css',
-  '/duoshop.js',
-  '/manifest.json',
-  '/assets/duo_logo.jpg',
-  '/assets/ubicacion.png'
+  './',
+  './index.html',
+  './duoshop.css',
+  './duoshop.js',
+  './manifest.json',
+  './assets/duo_logo.jpg',
+  './assets/ubicacion.png'
+  './terminos/index.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
